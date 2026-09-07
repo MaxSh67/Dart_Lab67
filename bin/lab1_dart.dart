1c5c17214ea0e01fbe4642c6830c5aeb5ca2ae98
MaxSh67 <maxhann0907@gmail.com>
2026-09-07 13:39:51 +0300

    twst

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,3 +1,47 @@
 void main(){
-List<String> fruits = ['яблоко'];
+String name = 'Максим';
+int age = 20;
+double height = 1.75;
+bool isStudent = true;
+
+print(name);
+print(age);
+print(height);
+print(isStudent);
+
+print("Привет, $name! Тебе $age лет.");
+print("Через 5 лет тебе будет ${age + 5} лет.");
+print("Рост: ${height} м, студентов: $isStudent");
+var score = 95;
+var language = 'Dart';
+print('$language: $score');
+const String appName = 'Lab67';
+final int startYear = 2026;
+print('$appName started in $startYear');
+
+String? city = null;
+if (city != null) {
+    print(
+        city.toUpperCase(),      
+    );
+}
+print(city?.toUpperCase());
+String? nickname = null;
+String display = 
+    nickname ?? 'Аноним';
+print(display);
+
+List<String> fruits = ['яблоко', 'банан', 'груша'];
+fruits.add('апельсин');
+print(fruits[0]);
+print(fruits.length);
+
+Map<String, dynamic> person = {'name': 'Артём', 'age': 20};
+print(person['name']);
+person['city'] = 'Волжский';
+
+Set<int> ids = {1, 2, 3, 2, 1};
+print(ids);
+print(ids.length);
+
 }
